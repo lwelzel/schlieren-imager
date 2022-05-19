b1793ef132243e0824f34163b017957f2424199f
Lukas Welzel <lukas.leonard.welzel@gmail.com>
2022-05-19 21:49:54 +0200

    fits and videos

--- bmp2other.py
+++ bmp2other.py
@@ -1,12 +1,58 @@
 import numpy as np
 import matplotlib as mpl
-import astropy
+from astropy.io import fits
 from pathlib import Path
+from natsort import natsorted
+from PIL import Image
+import matplotlib.pyplot as plt
+from moviepy.editor import ImageSequenceClip
 
-def bmp2fits(directory, **kwargs):
+def bmp2fits(directory, name="fits_from_bmp", **kwargs):
     path = Path(directory)
-    files = path.rglob(".bmp")
+    files = path.rglob("*.bmp")
+    files = [f.as_posix() for f in files]
+    files = natsorted(files)
 
-    
+    cube = np.zeros((len(files), *plt.imread(files[0]).shape), dtype=float)
+    for i, file in enumerate(files):
+        img = plt.imread(file)
+        # TODO: imgs might have channels that are not needed and should be removed
+        cube[i] = img
 
+    if len(cube.shape) == 4:
+        cube = np.moveaxis(cube, -1, 0)
+        hdu = fits.PrimaryHDU(cube[0])
+        hdul = fits.HDUList([hdu])
+        for i in range(1, cube.shape[0]):
+            hdu = fits.ImageHDU(cube[i])
+            hdul.append(hdu)
+    else:
+        hdu = fits.PrimaryHDU(cube)
+        hdul = fits.HDUList([hdu])
+
+    hdul.writeto(path / f'{name}.fits', overwrite=True)
     return
+
+def bmp2mp4(directory, name="mp4_from_bmp", fps=10, **kwargs):
+    path = Path(directory)
+    files = path.rglob("*.bmp")
+    files = [f.as_posix() for f in files]
+    files = natsorted(files)
+
+    clip = ImageSequenceClip(files, fps=fps)
+    # my moviepy needed a fix in
+    # "...\moviepy\decorators.py", line 118, in use_clip_fps_by_default
+    # the line
+    # names = inspect.getfullargspec(func).args[1:]
+    # needs to be changed to
+    # names = inspect.getfullargspec(f).args[1:]
+    # issue is known
+    # same needs to be done in
+    # "...\moviepy\decorators.py", line 79, in wrapper
+    clip.write_videofile(directory + f"/{name}.mp4", fps=fps, audio=False)
+
+
+
+if __name__ == '__main__':
+    bmp2fits("./example_bmps")
+    bmp2mp4("./example_bmps")
--- deflection.py
+++ deflection.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+### data on GD coefficients from 10.1016/j.combustflame.2018.06.022
+
 def get_rho(temperature, pressure=1., specific_gas_constant=1.):
     return pressure / (temperature * specific_gas_constant)
 
@@ -9,24 +11,112 @@ def get_refractive_index(k, rho):
 
 def main():
     temperature = np.linspace(20, 500, 100) + 273.15
-    r_medium = 148.19  # CO2
-    gd_coef = 2.26e-4
+
+    r_medium_co2 = 188.92  # CO2
+    gd_coef_co2 = 2.26e-4
+
+    r_medium_co = 296.84  # CO
+    gd_coef_co = 2.67e-4
+
+    r_medium_h20 = 461.5  # H2O
+    gd_coef_h20 = 3.12e-4
+
+    r_medium_h2 = 4124.2  # H2
+    gd_coef_h2 = 1.54e-4
+
+    r_medium_n2 = 296.80  # N2
+    gd_coef_n2 = 2.38e-4
+
+    r_medium_k2co3 = 0.  # K2CO3
+    gd_coef_k2co3 = 0.e-4
+
+    rs = np.array([r_medium_co2, r_medium_co, r_medium_h20, r_medium_h2, r_medium_n2])
+    gds = np.array([gd_coef_co2, gd_coef_co, gd_coef_h20, gd_coef_h2, gd_coef_n2])
+    weights = np.array([3.8, 5.21, 7.79, 3.07, 3.14])
+
     pressure = 101325
 
-    rhos = get_rho(temperature, pressure, r_medium)
-    n_medium = get_refractive_index(gd_coef, rhos)
+    medium_depth = 0.05
+
     n_air = 1.00027717
-    plt.plot(temperature, n_medium, c="red")
-    plt.axhline(1.00027717, c="k")
-    plt.axhline(1.000449, c="green")
-    plt.show()
+    n_co2 = 1.00045
+    n_co = 1.00035
+    n_n2 = 1.00029
 
-    plt.plot(temperature, (n_medium - 1.00027717) / 1.00027717, c="red")
-    plt.show()
+    rhos = np.zeros((5, temperature.size))
+    n_media = np.zeros((5, temperature.size))
+    angles = np.zeros((5, temperature.size))
+    deflections = np.zeros((5, temperature.size))
+    names = ["CO2", "CO", "H2O", "H2", "N2"]
 
-    deflection = (np.cos(n_medium - 1.00027717) / 1.00027717) * 2.4
-    print(deflection)
-    plt.plot(temperature, deflection, c="red")
+    fig, axes = plt.subplots(nrows=2, ncols=2,
+                             constrained_layout=True,
+                             figsize=(12, 12))  # figsize=(width, height)
+
+    axes = np.array(axes).flatten()
+    ax0, ax1, ax2, ax3 = axes
+
+    for i, (r, gd, name) in enumerate(zip(rs, gds, names)):
+
+        rho = get_rho(temperature, pressure, r)
+        n_medium = get_refractive_index(gd, rho)
+        angle = 2 * medium_depth * (n_medium - n_air) / n_air
+        deflection = np.tan(angle) * 2.4
+
+        rhos[i] = rho
+        n_media[i] = n_medium
+        angles[i] = angle
+        deflections[i] = deflection
+
+        ax0.plot(temperature, rho, label=name)
+        ax1.plot(temperature, n_medium, label=name)
+        ax2.plot(temperature, angle * 60., label=name)
+        ax3.plot(temperature, deflection * 1000., label=name)
+
+
+    mean_angle = np.average(angles, weights=weights, axis=0)
+    mean_deflection = np.average(deflections,  weights=weights, axis=0)
+
+    # mean_angle = np.mean(angles, axis=0)
+    # mean_deflection = np.mean(deflections, axis=0)
+
+    ax0.set_xlabel('T [K]')
+    ax0.set_ylabel(r'$\rho$ [kg/m$^3$]')
+    ax0.set_title('Density at 1 atm.')
+
+
+    ax1.axhline(n_air, ls="dashed", label="Air (ref at 273K)",
+                color=next(ax1._get_lines.prop_cycler)['color'])
+    ax1.axhline(n_co2, ls="dashed", label=r"CO2 (ref at 273K)",
+                color=next(ax1._get_lines.prop_cycler)['color'])
+    ax1.axhline(n_co, ls="dashed", label="CO (ref at 273K)",
+                color=next(ax1._get_lines.prop_cycler)['color'])
+    ax1.axhline(n_n2, ls="dashed", label="N2 (ref at 273K)",
+                color=next(ax1._get_lines.prop_cycler)['color'])
+    ax1.set_xlabel('T [K]')
+    ax1.set_ylabel('n [-]')
+    ax1.set_title('Refractive index.')
+    # ax1.set_ylim(1.00015, 1.00055)
+
+    ax2.plot(temperature, mean_angle * 60., ls="dotted", c="gray", label="avg angle")
+    ax2.axhline(0., ls="dashed",
+                color="k")
+    ax2.set_xlabel('T [K]')
+    ax2.set_ylabel(r'$\epsilon$ [arcmin]')
+    ax2.set_title('Deflection angle.')
+
+    ax3.plot(temperature, mean_deflection * 1000., ls="dotted", c="gray", label="avg deflection")
+    ax3.axhline(0., ls="dashed",
+                color="k")
+    ax3.set_xlabel('T [K]')
+    ax3.set_ylabel(r'$\Delta e$ [mm]')
+    ax3.set_title('Deflection at 2f.')
+
+    for ax in axes:
+        ax.legend()
+
+
+    fig.suptitle('Schlieren performance\n- BOE estimations at ~600 nm, 5cm medium -', fontsize=18, weight="bold")
     plt.show()
 
 main()
