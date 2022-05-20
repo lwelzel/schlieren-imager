5bfc3cd2f636ffdeb6994fdfb4a6c272f695f4cf
lukas_welzel <lukas.leonard.welzel@gmail.com>
2022-05-20 10:46:17 +0200

    flat image processing for fits Update .gitignore, bmp2other.py, and environment.yml

--- bmp2other.py
+++ bmp2other.py
@@ -13,7 +13,8 @@ def bmp2fits(directory, name="fits_from_bmp", **kwargs):
     files = [f.as_posix() for f in files]
     files = natsorted(files)
 
-    cube = np.zeros((len(files), *plt.imread(files[0]).shape), dtype=float)
+    # TODO: currently the output is ubyte but this really shouldnt be the case?!?
+    cube = np.zeros((len(files), *plt.imread(files[0]).shape), dtype=np.ubyte)
     for i, file in enumerate(files):
         img = plt.imread(file)
         # TODO: imgs might have channels that are not needed and should be removed
@@ -52,9 +53,29 @@ def bmp2mp4(directory, name="mp4_from_bmp", fps=10, **kwargs):
     # "...\moviepy\decorators.py", line 79, in wrapper
     clip.write_videofile(directory + f"/{name}.mp4", fps=fps, audio=False)
 
+
+def flat2flatfits(directory, name="flat_fits_from_bmp", **kwargs):
+    path = Path(directory)
+    files = path.rglob("*.bmp")
+    files = [f.as_posix() for f in files]
+    files = natsorted(files)
+
+    cube = np.zeros((len(files), *plt.imread(files[0]).shape), dtype=float)
+    for i, file in enumerate(files):
+        img = plt.imread(file)
+        # TODO: imgs might have channels that are not needed and should be removed
+        cube[i] = img
+
+    flat = np.median(cube, axis=0)
+    hdu = fits.PrimaryHDU(flat)
+    hdul = fits.HDUList([hdu])
+    hdul.writeto(path / f'{name}.fits', overwrite=True)
+
+
 def do_all_conv(dir):
     bmp2fits(dir)
-    bmp2mp4(dir)
+    # bmp2mp4(dir)
+    # flat2flatfits(dir)
 
 if __name__ == '__main__':
     do_all_conv("./spray_down")
